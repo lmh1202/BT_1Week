3a4b4e7d27e8b699c90983c0f04a2f5484f935f6
lmh1202 <lmhao1202@gmail.com>
2022-12-13 20:13:03 +0700

    UI, Filter, Lưu Local Storage

--- src/components/exercise-c1/component/BaiLamC1.tsx
+++ src/components/exercise-c1/component/BaiLamC1.tsx
@@ -1,28 +1,77 @@
 import { Menu, Button, Text, Tabs, Card, Input, ActionIcon, Center, Box, Checkbox, Group, Stack, TextInput } from "@mantine/core";
 import { IconChecklist, IconSortAscending, IconDots, IconTrash } from "@tabler/icons";
-import { v4 as uuidv4 } from 'uuid';
+import { parse, v4 as uuidv4 } from 'uuid';
+import { useLocalStorage } from "@mantine/hooks";
 import { KeyboardEvent, useState } from "react";
-import data from "../data/todo.type";
+import data, { Todo } from "../data/todo.type";
 
 export default function BaiLamC1() {
-  // const storeToDo = JSON.parse(localStorage.getItem('toDoList'))
-  const [toDoList, setTodoList] = useState([data]);
+  const getDataLocal: Todo[] = JSON.parse(localStorage.getItem("todoList"));
+  const [toDoList, setTodoList] = useState(getDataLocal);
   const [textInput, setTextInput] = useState("");
   const [checkInput, setCheckInput] = useState(true);
+  const [renderFilter, setRenderFilter] = useState("all");
+
+  //Input
   const onAddToDo = (e: KeyboardEvent) => {
     if (e.key === "Enter" && textInput) {
-      setTodoList(prev => {
-        const newToDo = [...prev, [{ uuid: uuidv4(), name: textInput, status: "pending" }]];
-        const jsonToDo = JSON.stringify(newToDo);
-        localStorage.setItem('toDoList', jsonToDo);
-        return [...prev, [{ uuid: uuidv4(), name: textInput, status: "pending" }]];
+      setTodoList(todo => {
+        const newToDo = [...todo, { uuid: uuidv4(), name: textInput, status: "pending" }];
+        return [...todo, { uuid: uuidv4(), name: textInput, status: "pending" }];
       });
       setTextInput("");
       setCheckInput(true);
+      console.log(toDoList);
     }
     else if (e.key === "Enter" && !textInput) {
       setCheckInput(false);
     }
+  };
+
+  //Completed
+  const checkCompleted = (index: number) => {
+    console.log(index);
+    const newTodoList = [...toDoList];
+    //change status
+    newTodoList[index] = {
+      ...newTodoList[index],
+      status: newTodoList[index].status === "pending" ? "completed" : "pending"
+    };
+    console.log(newTodoList);
+    setTodoList(newTodoList);
+  };
+
+  //Filter
+  const allFilter = () => {
+    setRenderFilter("all");
+    console.log(rending);
+  };
+
+  const pendingFilter = () => {
+    setRenderFilter("pending");
+    console.log(rending);
+  };
+
+  const completedFilter = () => {
+    setRenderFilter("completed");
+    console.log(rending);
+  };
+  const rending = toDoList.filter(todos => renderFilter === "all" || renderFilter === todos.status);
+
+  //Delete Todo
+  const deleteTodo = (index: number) => {
+    const newTodo = [...toDoList];
+    const deleteTodo = newTodo.filter(todo => {
+      return todo.uuid !== newTodo[index].uuid;
+    });
+    setTodoList(deleteTodo);
+  };
+
+  //Save Local Storage
+  const saveLocalStore = () => {
+    const listTodoStorage = [...toDoList];
+    const saveLocalStore = JSON.stringify(listTodoStorage);
+    localStorage.setItem("todoList", saveLocalStore);
   };
 
   return (
@@ -36,55 +85,43 @@ export default function BaiLamC1() {
               value={textInput}
               onChange={(e) => {
                 setTextInput(e.target.value);
-
               }}
               onKeyPress={(e) => onAddToDo(e)}
-
             />
           </Input.Wrapper>
 
-
           <Tabs>
             <Tabs.List grow>
-              <Tabs.Tab value="All">All</Tabs.Tab>
-              <Tabs.Tab value="Pending">Pending</Tabs.Tab>
-              <Tabs.Tab value="Complete">Complete</Tabs.Tab>
+              <Tabs.Tab value="all" onClick={allFilter}>All</Tabs.Tab>
+              <Tabs.Tab value="Pending" onClick={pendingFilter}>Pending</Tabs.Tab>
+              <Tabs.Tab value="Completed" onClick={completedFilter}>Completed</Tabs.Tab>
             </Tabs.List>
           </Tabs>
-          {toDoList.map((todo) => (
+          {rending.map((todo, index) => (
             <>
               <Box>
-                <Group grow>
-                  {todo.map((props) => (
-                    <>
-                      <Checkbox label={props.name} />
-                      <Menu>
-                        <Menu.Target>
-                          <ActionIcon>
-                            <IconDots size={18} />
-                          </ActionIcon>
-                        </Menu.Target>
+                <Group position='apart'>
+                  <Checkbox label={todo.name} onClick={() => checkCompleted(index)} />
+                  <Menu>
+                    <Menu.Target>
+                      <ActionIcon>
+                        <IconDots size={18} />
+                      </ActionIcon>
+                    </Menu.Target>
 
-                        <Menu.Dropdown>
-                          <Menu.Item icon={<IconTrash size={12} />}>Delete</Menu.Item>
-                        </Menu.Dropdown>
-                      </Menu>
-                    </>
-                  ))}
+                    <Menu.Dropdown>
+                      <Menu.Item onClick={() => deleteTodo(index)} icon={<IconTrash size={12} />}>Delete</Menu.Item>
+                    </Menu.Dropdown>
+                  </Menu>
                 </Group>
               </Box>
             </>
           ))}
-
-
-
         </Stack>
 
-
-        <Center>
-          <Button leftIcon={<IconChecklist size={18} />}>Save</Button>
+        <Center mt={50}>
+          <Button leftIcon={<IconChecklist size={18} />} onClick={saveLocalStore}>Save</Button>
         </Center>
-
       </Card>
     </>
   );
